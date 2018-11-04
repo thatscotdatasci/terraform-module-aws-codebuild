b3a9002e3b42bb37365243617991e7fa3ec9df0f
Alan Clark <alan@thatscottishdatascientist.com>
2018-11-04 15:05:50 +0000

    Correcting double closing curly brackets

--- modules/codebuild_s3/outputs.tf
+++ modules/codebuild_s3/outputs.tf
@@ -1,9 +1,9 @@
 output "id" {
   description = "The name (if imported via name) or ARN (if created via Terraform or imported via ARN) of the CodeBuild project"
-  value = "${aws_codebuild_project.this.id}}"
+  value = "${aws_codebuild_project.this.id}"
 }
 
 output "arn" {
   description = "The ARN of the CodeBuild project"
-  value = "${aws_codebuild_project.this.arn}}"
+  value = "${aws_codebuild_project.this.arn}"
 }
